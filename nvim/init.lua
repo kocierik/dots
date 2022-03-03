fe23429418969563139c335d6ec420ae52f25dd8
kocierik <kocierik@gmail.com>
2022-03-04 00:08:48 +0100

    feat: change structure

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,31 @@
-local ok, err = pcall(require, "core")
-
-if not ok then
-   error("Error loading core" .. "\n\n" .. err)
-end
+require'visimp'{
+  defaults = {
+    --relativenumber = false,
+  },
+  statusline = {
+    tabline = {
+      lualine_a = {'buffers'}
+    }
+  },
+  languages = {
+    'c',
+    'bash',
+    'css',
+    'html',
+    'java',
+    'javascript',
+    'json',
+    'latex',
+    'lua',
+    'python',
+  },
+  css = {
+    scss = true,
+  },
+  c = {
+    c = false
+  },
+  fugitive = {},
+  lspsignature = {},
+  theme = {'lifepillar/vim-gruvbox8', 'gruvbox8', 'dark'}
+}
