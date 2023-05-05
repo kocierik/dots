b99f2e6228eb60ccce174b378c842524b7bf5349
Erik <kocierik@gmail.com>
2023-05-05 11:55:24 +0200

    Update init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -1,4 +1,7 @@
 require 'visimp' {
+  defaults = {
+    foldmethod = 'marker'
+  },
   statusline = {
     tabline = {
       lualine_a = { 'buffers' }
@@ -14,19 +17,39 @@ require 'visimp' {
     'latex',
     'lua',
     'python',
-    'go'
+    'hcl',
   },
   css = {
     scss = true,
   },
   binds = {
-    [{ mode = 'n', bind = '<C-Left>' }] = 'bprev',
-    [{ mode = 'n', bind = '<C-Right>' }] = 'bnext'
+    [{ mode = 'n', bind = '<C-P>' }] = ':bprev<cr>',
+    [{ mode = 'n', bind = '<C-N>' }] = ':bnext<cr>',
+    [{ mode = 'n', bind = '<C-T>' }] = ':NvimTreeToggle<cr>'
   },
-  fugitive = {},
   autopairs = {},
+  cmp = {},
+  comment = {},
+  fugitive = {},
+  gitsigns = {},
+  snippet = {},
+  outline = {},
+  icons = {},
   lspformat = {},
   treesitter = {},
+  nvimtree = {
+    icons = true,
+    sort_by = "case_sensitive",
+    view = {
+      width = 30
+    },
+    renderer = {
+      group_empty = true,
+    },
+    filters = {
+      dotfiles = true,
+    }
+  },
   theme = {
     package = 'bluz71/vim-nightfly-guicolors',
     colorscheme = 'nightfly',
